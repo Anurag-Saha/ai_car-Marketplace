f30ac68db79841110c6e7e908d05b21ba9765ef2
Anurag <anuragsaha9183@gmail.com>
2025-05-07 08:11:55 +0530

    problems

--- next.config.mjs
+++ next.config.mjs
@@ -15,12 +15,6 @@ const nextConfig = {
         ];
         
     },
-    eslint: {
-      ignoreDuringBuilds: true,
-    },
-    typescript:{
-      ignoreBuildErrors: true
-    },
 
     experimental: {
         serverComponentsHmrCache:false, // default to true 
